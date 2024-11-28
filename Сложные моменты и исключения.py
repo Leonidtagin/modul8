433d365e7f278aa45bd6bb29a69d94addb3da660
Игорёха <leonidtagin401@gmail.com>
2024-11-29 04:19:09 +1100

    ...

--- Сложные моменты и исключения.py
+++ Сложные моменты и исключения.py
@@ -1,4 +1,4 @@
-def prrsonal_sum(*numbers):
+def personal_sum(*numbers):
     result = 0
     incorrect_data = 0
     for i in numbers:
@@ -13,18 +13,23 @@ def prrsonal_sum(*numbers):
     return result, incorrect_data
 
 def calculate_avarage(*numbers):
-    if isinstance(*numbers, int):
-        return None
     try:
-        tuple_pers_sum = personal_sum(*numbers)
+        if not isinstance(*numbers, int):
+            print(f'В numbers записан некорректный тип данных')
+            return None
 
-        return tuple_pers_sum[0] / (len(*numbers) - tuple_pers_sum[1])
+        total_sum, incorrect_data = personal_sum(numbers)
+        count = len(numbers) - incorrect_data
+        avarage = total_sum / count
+        return avarage
+
     except ZeroDivisionError:
         return 0
     except TypeError:
-        return f'В numbers записан некорректный тип данных'
+        print('В numbers записан некорректный тип данных')
+        return None
 
 print(f'результат 1: {calculate_avarage("1, 2, 3")}')
 print(f'результат 2: {calculate_avarage([1, "строка", 3, "Еще строка"])}')
 print(f'результат 3: {calculate_avarage(567)}')
-print(f'результат 4: {calculate_avarage(42, 15, 36, 13])}')
+print(f'результат 4: {calculate_avarage([42, 15, 36, 13])}')
